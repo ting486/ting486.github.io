1836545d4841a92d4bec372e5c78ef2684de62d8
Ting Guo <ting1107g@gmail.com>
2023-01-29 19:51:20 -0800

    Add project filtering by programming language

--- static/my_script.js
+++ static/my_script.js
@@ -1,1 +1,41 @@
+const arr = [];
+let counter = 1;
+const languagesAll = document.querySelectorAll(".language");
+var rad = document.projectFilterForm.myLanguage;
+
+
+// open all external links in new tab
 $("a:not(#nav a)").attr("target", "_blank");
+
+// set up a list of skills used in projects
+for (const lgg of languagesAll) {
+    const lggClean = lgg.textContent.replace(/ *\([^)]*\) */g, "");
+    const lList = lggClean.split(", ");
+
+    arr.push({
+        id: counter++,
+        languages: lList
+    });
+}
+
+// project filtering by programming language
+for (var i = 0; i < rad.length; i++) {
+    rad[i].addEventListener('change', function() {
+        const l = this.value;
+
+        if (l != "All") {
+            for (let i = 0; i < arr.length; i++) {
+                const ar = arr[i];
+                if (ar.languages.includes(l)) {
+                    $(`#projects .card:nth-child(${i+1})`).css("display", "block");
+                }
+                else {
+                    $(`#projects .card:nth-child(${i+1})`).css("display", "none");
+                }
+            }
+        }
+        else {
+            $("#projects .card").css("display", "block");
+        }
+    });
+}
